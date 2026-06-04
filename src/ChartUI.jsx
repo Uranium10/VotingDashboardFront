9b69e48133aed16e5565b5cf269d55365d82b20e
uran <kdm10ho@naver.com>
2026-06-04 17:09:49 +0900

    by auto uploader

--- src/ChartUI.jsx
+++ src/ChartUI.jsx
@@ -6,7 +6,7 @@ import {
 import './ChartUI.css';
 
 // 💡 안전한 서버 주소 하드코딩
-const API = 'https://port-0-votingdashboardserver-mpz7fuz0e609fe68.sel3.cloudtype.app/';
+const API = 'https://port-0-votingdashboardserver-mpz7fuz0e609fe68.sel3.cloudtype.app';
 
 function useFetch(url) {
   const [data, setData] = useState(null);
